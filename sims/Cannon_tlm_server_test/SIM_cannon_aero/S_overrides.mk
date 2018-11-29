fdc202f8c73e4aeacac8c3978e2020d4867d03b7
Danrae Pray <danrae.pray@metecs.com>
2018-11-29 16:27:43 -0600

    wip

--- sims/Cannon_tlm_server_test/SIM_cannon_aero/S_overrides.mk
+++
@@ -1,2 +0,0 @@
-TRICK_CFLAGS += -I../models
-TRICK_CXXFLAGS += -I../models
--- sims/Cannon_tlm_server_test/SIM_cannon_eulercromer/RUN_test/input.py
+++
@@ -1,5 +0,0 @@
-
-execfile("Modified_data/cannon.dr")
-dyn_integloop.getIntegrator(trick.Euler_Cromer, 4)
-dyn_integloop.integ_sched.set_verbosity( 1)
-trick.exec_set_terminate_time(5.2)
