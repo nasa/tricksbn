3bf71d755b6a818887c61a4a157db5e7c45a9703
Danrae Pray <danrae.pray@metecs.com>
2018-12-11 15:18:03 -0600

    wip

--- sims/models/tlm_server/src/tlm_server.cpp
+++ sims/models/tlm_server/src/tlm_server.cpp
@@ -2,12 +2,25 @@
 
 #include <iostream>
 
+#include <QDir>
+#include <QString>
+#include <QStringList>
+
 void InitTlmServer(TelemetryServerConfig *config)
 {
     // you can grab all inputs from the python input file and
     // do all your server initialization based on that here...
 
     std::cout << "TELEMETRY_SERVER: TVM File Directory = " << config->tvmFileDir << std::endl;
+
+    QDir tvmFileDir(QString(config->tvmFileDir.c_str()));
+
+    QStringList tvmFiles = tvmFileDir.entryList(QStringList() << "*.tvm", QDir::Files);
+
+    for (int i = 0; i < tvmFiles.length(); ++i)
+    {
+        std::cout << tvmFiles.at(i).toLocal8Bit().constData() << std::endl;
+    }
 
     if (config->prototypes.HasType("bob"))
         std::cout << "hello bob o/" << std::endl;
