48d7eec14905c2ccc2e0d733acc0335b4954f604
Danrae Pray <danrae.pray@metecs.com>
2018-12-11 14:27:47 -0600

    wip

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 *** NOTES:
 
-    - You need to set your LD_LIBRARY_PATH to the location of your library directory for the Qt libs
+    - You need to set your LD_LIBRARY_PATH to the location of your library directory for the Qt libs *before* building b.c. trick-CP will try to run exe after build
--- sims/SIM_cannon_numeric/S_overrides.mk
+++ sims/SIM_cannon_numeric/S_overrides.mk
@@ -4,10 +4,10 @@ TRICK_CXXFLAGS = -I../models
 
 TRICK_CXXFLAGS += -I/users/dspray1/Qt/5.11.1/gcc_64/include -I/users/dspray1/Qt/5.11.1/gcc_64/include/QtCore -I/users/dspray1/Qt/5.11.1/gcc_64/mkspecs/linux-g++
 
-TRICK_CXXFLAGS += -Wall -D_REENTRANT -DQT_DEPRECATED_WARNINGS -DQT_NO_DEBUG -DQT_CORE_LIB
+TRICK_CXXFLAGS += -I../../extern/protobetter/protobetter_dynamic_lib/include
 
-TRICK_CXXFLAGS += -std=c++0x -fPIC -m64 -Wl,-O1 
+TRICK_CXXFLAGS += -D_REENTRANT -DQT_DEPRECATED_WARNINGS -DQT_NO_DEBUG -DQT_CORE_LIB
+
+TRICK_CXXFLAGS += -std=c++0x -fPIC -m64
 
 TRICK_USER_LINK_LIBS += -L/users/dspray1/Qt/5.11.1/gcc_64/lib -lQt5Core -lpthread -licui18n -licuuc -licudata
-
-TRICK_CXXFLAGS += -I../../extern/protobetter/protobetter_dynamic_lib/include
