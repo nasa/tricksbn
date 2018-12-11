562fb5490f4c78815275100172170753058a878b
Danrae Pray <danrae.pray@metecs.com>
2018-12-11 14:17:44 -0600

    wip

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-TODO: wip
+*** NOTES:
+
+    - You need to set your LD_LIBRARY_PATH to the location of your library directory for the Qt libs
--- sims/SIM_cannon_numeric/S_overrides.mk
+++ sims/SIM_cannon_numeric/S_overrides.mk
@@ -1,15 +1,13 @@
 
 TRICK_CFLAGS += -I../models
-TRICK_CXXFLAGS += -I../models
+TRICK_CXXFLAGS = -I../models
 
-TRICK_CXXFLAGS += -I/users/dspray1/Qt/5.11.1/gcc_64/include -I/users/dspray1/Qt/5.11.1/gcc_64/include/QtCore
+TRICK_CXXFLAGS += -I/users/dspray1/Qt/5.11.1/gcc_64/include -I/users/dspray1/Qt/5.11.1/gcc_64/include/QtCore -I/users/dspray1/Qt/5.11.1/gcc_64/mkspecs/linux-g++
 
 TRICK_CXXFLAGS += -Wall -D_REENTRANT -DQT_DEPRECATED_WARNINGS -DQT_NO_DEBUG -DQT_CORE_LIB
 
 TRICK_CXXFLAGS += -std=c++0x -fPIC -m64 -Wl,-O1 
 
-# TODO: this doesn't work - it keeps finding the system version of Qt5Core first I think...
-#TRICK_USER_LINK_LIBS = "-L/users/dspray1/Qt/5.11.1/gcc_64/lib"
-#TRICK_USER_LINK_LIBS += -lQt5Core -lpthread
+TRICK_USER_LINK_LIBS += -L/users/dspray1/Qt/5.11.1/gcc_64/lib -lQt5Core -lpthread -licui18n -licuuc -licudata
 
 TRICK_CXXFLAGS += -I../../extern/protobetter/protobetter_dynamic_lib/include
