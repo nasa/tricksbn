fb64ca54b07a912b65ec8dba64aa34453a2db4a5
Danrae Pray <danrae.pray@metecs.com>
2018-12-17 11:15:32 -0600

    wip

--- sims/SIM_cannon_numeric/S_overrides.mk
+++ sims/SIM_cannon_numeric/S_overrides.mk
@@ -8,6 +8,7 @@ TRICK_CXXFLAGS += -I${QT_INSTALL_DIR}/include -I${QT_INSTALL_DIR}/include/QtCore
 
 TRICK_CXXFLAGS += -I../../extern/protobetter/protobetter_dynamic_lib/include
 TRICK_CXXFLAGS += -I../../extern/ccsds_header_lib/include
+TRICK_CFLAGS += -I../../extern/ccsds_header_lib/include
 
 TRICK_CXXFLAGS += -D_REENTRANT -DQT_DEPRECATED_WARNINGS -DQT_NO_DEBUG -DQT_CORE_LIB
 
