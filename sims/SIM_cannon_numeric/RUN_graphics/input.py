106b69eb97db55d2021319eada2570d5b755947b
Danrae Pray <danrae.pray@metecs.com>
2019-02-07 09:50:51 -0600

    updated cannon RUN_graphics/input.py to show how to integrate w/ existing model input files

--- sims/SIM_cannon_numeric/RUN_graphics/input.py
+++ sims/SIM_cannon_numeric/RUN_graphics/input.py
@@ -4,10 +4,40 @@ execfile("Modified_data/realtime.py")
 # Start the Cannonball Graphics Client
 #==========================================
 
-tlmServer.configData.tvmFileDir = "../models/bob"
-
-varServerPort = trick.var_server_get_port();
 CannonDisplay_path = "../models/graphics/dist/CannonDisplay.jar"
+
+#==========================================
+# Trick-SBN Config
+#==========================================
+
+tlmServer.configData.tvmFileDir = "/users/dspray1/code/tricksbn/sims/SIM_cannon_numeric/bittylicious_tvm_files"
+tlmServer.configData.prototypeFileDir = "/users/dspray1/code/tricksbn/sims/SIM_cannon_numeric/bittylicious_ptype_files"
+
+tlmServer.configData.qsbnJsonConfig = '''
+
+{
+    "protocol":"stream",
+
+    "timeout":3000,
+
+    "sbnHostConfig":
+    {
+        "cpuId":2,
+        "ip":"127.0.0.1",
+        "port":15821
+    },
+
+    "sbnPeerConfig":
+    [
+        { "cpuId":1, "ip":"127.0.0.1", "port":15820 }
+    ]
+}
+
+'''
+
+# only set this to true if you want this to run as if it were just simulating
+# a trick model producing/consuming data on SBN
+tlmServer.configData.useSimulatedTrickBackend = False 
 
 if (os.path.isfile(CannonDisplay_path)) :
     CannonDisplay_cmd = "java -jar " \
