143143609c2482a8bfe089975d42ab12b4fe02fa
Danrae Pray <danrae.pray@metecs.com>
2019-02-12 15:20:15 -0600

    making this stuff configurable and separating out build cfg for tlm server into it's own *_overrides.mk

--- sims/SIM_cannon_numeric/S_overrides.mk
+++ sims/SIM_cannon_numeric/S_overrides.mk
@@ -3,24 +3,4 @@
 TRICK_CFLAGS += -I../models 
 TRICK_CXXFLAGS = -I../models
 
-# TRICK_CXXFLAGS += -I/users/dspray1/Qt/5.11.1/gcc_64/include -I/users/dspray1/Qt/5.11.1/gcc_64/include/QtCore -I/users/dspray1/Qt/5.11.1/gcc_64/mkspecs/linux-g++
-TRICK_CXXFLAGS += -I${QT_INSTALL_DIR}/include -I${QT_INSTALL_DIR}/include/QtCore -I${QT_INSTALL_DIR}/include/QtNetwork -I${QT_INSTALL_DIR}/mkspecs/linux-g++
-
-TRICK_CXXFLAGS += -DTRICK_SBN_DEBUG_LOGGING_ENABLED
-
-TRICK_CXXFLAGS += -DPROTOBETTER_DYNAMIC_LIB_OBJECT
-
-TRICK_CXXFLAGS += -I../../extern/protobetter/protobetter_dynamic_lib/include
-TRICK_CXXFLAGS += -I../../extern/ccsds_header_lib/include
-TRICK_CXXFLAGS += -I../../extern/trick_sbn_lib
-
-TRICK_CFLAGS += -I../../extern/ccsds_header_lib/include
-
-
-
-TRICK_CXXFLAGS += -D_REENTRANT -DQT_DEPRECATED_WARNINGS -DQT_NO_DEBUG -DQT_CORE_LIB
-
-TRICK_CXXFLAGS += -std=c++0x -fPIC -m64
-
-TRICK_USER_LINK_LIBS += -L${QT_INSTALL_DIR}/lib
-TRICK_USER_LINK_LIBS += -lQt5Core -lQt5Network -lpthread -licui18n -licuuc -licudata
+include ${TRICK_SBN_SIM_DIR}/S_modules/tricksbn_overrides.mk
---
+++ sims/SIM_tlm_server/S_modules/tricksbn_overrides.mk
@@ -0,0 +1,20 @@
+TRICK_CXXFLAGS += -I${QT_INSTALL_DIR}/include -I${QT_INSTALL_DIR}/include/QtCore -I${QT_INSTALL_DIR}/include/QtNetwork -I${QT_INSTALL_DIR}/mkspecs/linux-g++
+
+TRICK_CXXFLAGS += -DTRICK_SBN_DEBUG_LOGGING_ENABLED
+
+TRICK_CXXFLAGS += -DPROTOBETTER_DYNAMIC_LIB_OBJECT
+
+TRICK_CXXFLAGS += -I${TRICK_SBN_LIB_DIR}/protobetter/protobetter_dynamic_lib/include
+TRICK_CXXFLAGS += -I${TRICK_SBN_LIB_DIR}/ccsds_header_lib/include
+TRICK_CXXFLAGS += -I${TRICK_SBN_LIB_DIR}/trick_sbn_lib
+
+TRICK_CFLAGS += -I${TRICK_SBN_LIB_DIR}/ccsds_header_lib/include
+
+
+
+TRICK_CXXFLAGS += -D_REENTRANT -DQT_DEPRECATED_WARNINGS -DQT_NO_DEBUG -DQT_CORE_LIB
+
+TRICK_CXXFLAGS += -std=c++0x -fPIC -m64
+
+TRICK_USER_LINK_LIBS += -L${QT_INSTALL_DIR}/lib
+TRICK_USER_LINK_LIBS += -lQt5Core -lQt5Network -lpthread -licui18n -licuuc -licudata
