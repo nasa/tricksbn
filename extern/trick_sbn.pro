d1ebdfe646b2d8404de9b96e09394ea3fb83cb1a
Deep Mukherji <shubhodeep.mukherji@nasa.gov>
2019-02-28 13:34:43 -0600

    Enforced ordering in trick_sbn qmake and added example SiM_cannon_numeric producer

--- extern/trick_sbn.pro
+++ extern/trick_sbn.pro
@@ -4,3 +4,5 @@ SUBDIRS += \
     trick_sbn_lib \
     trick_sbn_standalone \
     trick_sbn_test
+
+CONFIG += ordered
--- sims/SIM_cannon_numeric/setvars.sh
+++ sims/SIM_cannon_numeric/setvars.sh
@@ -1,4 +1,4 @@
-export TRICK_SBN_SIM_DIR=/users/dspray1/code/tricksbn/sims/SIM_tlm_server
-export TRICK_SBN_LIB_DIR=/users/dspray1/code/tricksbn/extern
-export QT_INSTALL_DIR=/users/dspray1/Qt/5.11.1/gcc_64
+export TRICK_SBN_SIM_DIR=$( readlink -e ../SIM_tlm_server )
+export TRICK_SBN_LIB_DIR=$( readlink -e ../../extern )
+export QT_INSTALL_DIR=${HOME}/Qt/5.12.1/gcc_64
 export LD_LIBRARY_PATH=${QT_INSTALL_DIR}/lib
